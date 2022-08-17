c00c2d9d15abc9860a53f5a1c3071aa637a68d0c
vijay <91136722+vijaydeveloper18@users.noreply.github.com>
2022-08-17 20:25:43 +0530

    problem for reverse the word using javascript

---
+++ wordrev.js
@@ -0,0 +1,8 @@
+var str = "I like this program very much";
+var array = Array.from(str);
+console.log(array);
+var text = ['I', 'like', 'this', 'program', 'very', 'much'];
+for(i=0;i<=text.length;i++){
+    var rev = text.reverse();
+}
+console.log(rev.join(" "));
