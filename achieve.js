badd947c8ce5c086d971c6791e5b0c833caba544
vijay <91136722+vijaydeveloper18@users.noreply.github.com>
2022-08-24 19:07:02 +0530

    project-1 : Achieve page codes

---
+++ achieve.js
@@ -0,0 +1,9 @@
+function openfb(){
+   window.location.assign("https://www.facebook.com/");
+}
+function openinsta(){
+   window.location.assign("https://www.instagram.com/");
+}
+function opentwit(){
+   window.location.assign("https://www.twitter.com/");
+}
